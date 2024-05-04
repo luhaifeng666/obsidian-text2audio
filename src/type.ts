e2f1b29e8621afea9045b894a440390e6bf89296
luhaifeng666 <youzui@hotmail.com>
2024-05-05 00:13:48 +0800

    feat: 新增音频格式支持

--- src/type.ts
+++ src/type.ts
@@ -1,20 +1,21 @@
 export type ConfigKeys =
-	| "text"
-	| "filename"
-	| "key"
-	| "region"
-	| "filePath"
-	| "voice"
-	| "type";
+  | "text"
+  | "filename"
+  | "key"
+  | "region"
+  | "filePath"
+  | "voice"
+  | "audioFormat"
+  | "type";
 export type MessageType = "success" | "error" | "warning";
 export type SettingType = "name" | "desc" | "key";
 export type ConfigurationType = "key" | "region" | "directory" | "language";
 export interface SettingInputConfig {
-	placeholder: string;
-	callback?: (value: string) => void;
+  placeholder: string;
+  callback?: (value: string) => void;
 }
 export type SettingConfig = Record<SettingType, string> & {
-	inputConfig?: SettingInputConfig;
-	type: "text" | "select";
-	options?: Record<string, string>;
+  inputConfig?: SettingInputConfig;
+  type: "text" | "select";
+  options?: Record<string, string>;
 };
